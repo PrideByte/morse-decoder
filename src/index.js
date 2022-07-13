74034491532ad355d6ccd3a6a9fc06efff6a02fe
PrideByte <alpha5.2@mail.ru>
2022-07-13 22:32:14 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const splittedExpr = expr.match(/[\d*]{1,10}/g).map(el => el.length >= 10 ? el : '0'.repeat(10 - el.length) + el);
+    return splittedExpr.map(el => {
+        el = el == '*'.repeat(10)
+            ? ' '
+            : el.match(/\d{2}/g).map(el => {
+                if (el == '10') return '.';
+                if (el == '11') return '-';
+                return '';
+              }).join('');
+        return el == ' ' ? el : MORSE_TABLE[el];
+    }).join('');
 }
 
 module.exports = {
